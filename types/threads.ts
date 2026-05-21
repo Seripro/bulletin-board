d248437145d31780e71ff91b24c0362b02fad844
keigo <2364138keigo@gmail.com>
2026-05-21 18:44:07 +0900

    threads, profilesの型を定義

---
+++ types/threads.ts
@@ -0,0 +1,3 @@
+import { Database } from "@/database.types";
+
+export type ThreadsType = Database["public"]["Tables"]["threads"]["Row"];
---
+++ types/user.ts
@@ -0,0 +1,3 @@
+import { Database } from "@/database.types";
+
+export type UserType = Database["public"]["Tables"]["profiles"]["Row"];
