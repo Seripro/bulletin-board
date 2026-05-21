24fb84e624d14e5b0df76f786fc06ca1b114a1f9
keigo <2364138keigo@gmail.com>
2026-05-21 12:53:36 +0900

    認証のコンテキストを作成

---
+++ contexts/AuthContext.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { Session } from "@supabase/supabase-js";
+import { createContext } from "react";
+
+type ValueType = {
+  session: Session | null;
+  userId: string | null;
+  signInWithPassword: (
+    email: string,
+    password: string,
+  ) => Promise<string | null>;
+  signUpWithPassword: (
+    email: string,
+    password: string,
+  ) => Promise<string | null>;
+  signOut: () => Promise<void>;
+};
+
+export const AuthContext = createContext<ValueType | null>(null);
