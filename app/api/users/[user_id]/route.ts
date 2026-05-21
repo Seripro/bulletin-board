1283055cc84f8c0ef1bb1948bbecfbce2267a675
keigo <2364138keigo@gmail.com>
2026-05-21 18:46:17 +0900

    user_idが一致するprofilesを取得するエンドポイントを定義

---
+++ app/api/users/[user_id]/route.ts
@@ -0,0 +1,19 @@
+import { supabase } from "@/utils/supabase";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { user_id: string } },
+) {
+  const { user_id } = await params;
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", user_id)
+    .single();
+  if (error) {
+    throw error;
+  } else {
+    return NextResponse.json(data, { status: 200 });
+  }
+}
