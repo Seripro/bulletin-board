9e60abcff4ed31a83b907909b018e69765429d81
keigo <2364138keigo@gmail.com>
2026-05-21 16:23:21 +0900

    それぞれログイン状態によってアクセスを制限するlayoutを作成

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { session, loading } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (!loading && !session) {
+      router.replace("/login");
+    }
+  }, [loading, session, router]);
+  if (loading) return <p>loading...</p>;
+  return <>{children}</>;
+}
