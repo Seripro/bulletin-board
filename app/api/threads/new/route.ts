c2718e5c686f9d76d686eb900e7a24aafb1f535a
keigo <2364138keigo@gmail.com>
2026-05-22 09:18:31 +0900

    リクエストヘッダーのトークンからバックエンドでユーザーIDを特定する方法に変えた。（これ意味あるのかな）

--- app/api/threads/new/route.ts
+++ app/api/threads/new/route.ts
@@ -2,9 +2,23 @@ import { supabase } from "@/utils/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  console.log(body);
-  const { data, error } = await supabase.from("threads").insert(body);
+  const auth = request.headers.get("authorization");
+  const token = auth?.replace("Bearer ", "");
+  if (!token)
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+
+  const { data: userData, error: userError } =
+    await supabase.auth.getUser(token);
+  if (userError || !userData.user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const { title, content } = await request.json();
+  const { data, error } = await supabase.from("threads").insert({
+    title,
+    content,
+    user_id: userData.user.id,
+  });
   if (error) {
     throw error;
   } else {
